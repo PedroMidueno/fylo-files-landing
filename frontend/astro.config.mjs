374823b6a80ba28e6b31254869c9c652b2488bf6
PedroMidueno <pedromidueno.dev@gmail.com>
2025-10-19 13:20:05 -0600

    Add SSR

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,12 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  output: 'server'
 });
